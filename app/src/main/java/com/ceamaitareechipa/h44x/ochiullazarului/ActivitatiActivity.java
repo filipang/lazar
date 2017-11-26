45e7b426dfaa55d712987c21b04d6422bba93bf6
SerbanJinga <33983121+SerbanJinga@users.noreply.github.com>
2017-11-26 10:54:56 +0200

    DDDDD

--- app/src/main/java/com/ceamaitareechipa/h44x/ochiullazarului/ActivitatiActivity.java
+++ app/src/main/java/com/ceamaitareechipa/h44x/ochiullazarului/ActivitatiActivity.java
@@ -1,7 +1,10 @@
 package com.ceamaitareechipa.h44x.ochiullazarului;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivitatiActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class ActivitatiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activitati);
+
+        final Button buttonP = findViewById(R.id.detalii);
+        buttonP.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivitatiActivity.this, InformatiiActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
