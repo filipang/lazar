5de66a32f76087741875cef558e46f4d8dc4a6cd
SerbanJinga <33983121+SerbanJinga@users.noreply.github.com>
2017-11-26 10:11:01 +0200

    s o futi pe ma ta

--- app/src/main/java/com/ceamaitareechipa/h44x/ochiullazarului/SignUp.java
+++ app/src/main/java/com/ceamaitareechipa/h44x/ochiullazarului/SignUp.java
@@ -1,7 +1,10 @@
 package com.ceamaitareechipa.h44x.ochiullazarului;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        final Button buttonS = findViewById(R.id.orar);
+        buttonS.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
