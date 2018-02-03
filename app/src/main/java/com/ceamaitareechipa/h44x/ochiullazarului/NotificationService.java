560969e7d1985a3372a082f3f5ffd471533e884c
filipang <33981940+filipang@users.noreply.github.com>
2018-02-03 23:42:09 +0200

    Notificarile oficial arata corect ora, respectand orarul clasei mele (9C), nu am putut testa complet insa, voi astepta ziua de luni si voi vedea daca imi respecta orarul fara probleme.

--- app/src/main/java/com/ceamaitareechipa/h44x/ochiullazarului/NotificationService.java
+++ app/src/main/java/com/ceamaitareechipa/h44x/ochiullazarului/NotificationService.java
@@ -65,48 +65,114 @@ public class NotificationService extends Service {
 
     public void UpdateOrarNotification(){
         String text = "", title = "LZR App Orar";
+        boolean notify = true;
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
         Calendar calendar = Calendar.getInstance();
         int dayValue = calendar.get(Calendar.DAY_OF_WEEK) - 1;
-        int timeValue = calendar.get(Calendar.HOUR_OF_DAY) * 60 + calendar.get(Calendar.MINUTE);
+        int timeValue = calendar.get(Calendar.HOUR_OF_DAY) * 60 * 60 + calendar.get(Calendar.MINUTE)*60 + calendar.get(Calendar.SECOND);
         int hourIndex, offset;
-        if(timeValue <= 18*60 + 30 && timeValue >= 7*60 + 30){
-            if(timeValue < 12*60+45 && timeValue >= 12*60 + 25){
-                hourIndex = (timeValue - 7*60 - 30)/50 + 1;
-                offset = 12*60 + 45 - timeValue;
-            }
-            else{
-                if(timeValue >= 12*60 + 45){
-                    timeValue = timeValue - 15;
-                    hourIndex = (timeValue - 7*60 - 30)/50 + 1;
-                    offset = (timeValue - 7*60 - 30)%50;
+        if(timeValue <= TimeSec(18,30) && timeValue >= TimeSec(7,30)){
+            if(timeValue < 12*60*60+45*60 && timeValue >= 12*60*60 + 25*60){
+                offset = 12*60*60 + 45*60 - timeValue;
+                try {
+                    if (!HomrActivity.orarJSON.getJSONObject("9c").getJSONObject("" + dayValue).optString("" + (7)).equals("")) {
+                        if (offset/60 == 19) {
+                            if (60 - offset % 60 == 1) {
+                                text = "Ora de " + HomrActivity.orarJSON.getJSONObject("9c").getJSONObject("" + dayValue).optString("" + 7) + " incepe peste " + offset/ 60 + " minut si " + offset % 60 + " secunda.";
+                            } else {
+                                text = "Ora de " + HomrActivity.orarJSON.getJSONObject("9c").getJSONObject("" + dayValue).optString("" + 7) + " incepe peste " + offset / 60 + " minut si " + offset % 60 + " secunde.";
+
+                            }
+                        } else {
+                            if (60 - offset % 60 == 1) {
+                                text = "Ora de " + HomrActivity.orarJSON.getJSONObject("9c").getJSONObject("" + dayValue).optString("" + 7) + " incepe peste " + offset / 60 + " minute si " + offset % 60 + " secunda.";
+                            } else {
+                                text = "Ora de " + HomrActivity.orarJSON.getJSONObject("9c").getJSONObject("" + dayValue).optString("" + 7) + " incepe peste " + offset / 60 + " minute si " + offset % 60 + " secunda.";
+                            }
+                        }
+                    } else {
+                        notify = false;
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
-                else{
-                    hourIndex = (timeValue - 7*60 - 30)/50 + 1;
-                    offset = (timeValue - 7*60 - 30)%50;
-                }
             }
-            try {
-                text = HomrActivity.orarJSON.getJSONObject("9c").getJSONObject("" + dayValue).optString("" + hourIndex) + " acum " + offset/60 + " minute si " + offset % 60 + " secunde.";
-            } catch (JSONException e) {
-                e.printStackTrace();
+            else {
+                if (timeValue >= TimeSec(12, 45, 0)) {
+                    timeValue = timeValue - 15 * 60;
+                    hourIndex = (timeValue - 7 * 60 * 60 - 30 * 60) / (50 * 60) + 1;
+                    offset = (timeValue - 7 * 60 * 60 - 30 * 60) % (50 * 60);
+                } else {
+                    hourIndex = (timeValue - 7 * 60 * 60 - 30 * 60) / (50 * 60) + 1;
+                    offset = (timeValue - 7 * 60 * 60 - 30 * 60) % (50 * 60);
+                }
+                try {
+                    if (offset < 45 * 60) {
+                        if (!HomrActivity.orarJSON.getJSONObject("9c").getJSONObject("" + dayValue).optString("" + hourIndex).equals("")) {
+                            if (offset / 60 == 1) {
+                                if (offset % 60 == 1) {
+                                    text = "Ora de " + HomrActivity.orarJSON.getJSONObject("9c").getJSONObject("" + dayValue).optString("" + hourIndex) + " a inceput acum " + offset / 60 + " minut si " + offset % 60 + " secunda.";
+                                } else {
+                                    text = "Ora de " + HomrActivity.orarJSON.getJSONObject("9c").getJSONObject("" + dayValue).optString("" + hourIndex) + " a inceput acum " + offset / 60 + " minut si " + offset % 60 + " secunde.";
+
+                                }
+                            } else {
+                                if (60 - offset % 60 == 1) {
+                                    text = "Ora de " + HomrActivity.orarJSON.getJSONObject("9c").getJSONObject("" + dayValue).optString("" + hourIndex) + " a inceput acum " + offset / 60 + " minute si " + offset % 60 + " secunda.";
+                                } else {
+                                    text = "Ora de " + HomrActivity.orarJSON.getJSONObject("9c").getJSONObject("" + dayValue).optString("" + hourIndex) + " a inceput acum " + offset / 60 + " minute si " + offset % 60 + " secunde.";
+                                }
+                            }
+                        } else {
+                            notify = false;
+                        }
+                    } else {
+                        if (!HomrActivity.orarJSON.getJSONObject("9c").getJSONObject("" + dayValue).optString("" + (hourIndex + 1)).equals("")) {
+                            if (offset/60 == 49) {
+                                if (60 - offset % 60 == 1) {
+                                    text = "Ora de " + HomrActivity.orarJSON.getJSONObject("9c").getJSONObject("" + dayValue).optString("" + (hourIndex + 1)) + " incepe peste " + (50 - offset / 60) + " minut si " + (60 - offset % 60) + " secunda.";
+                                } else {
+                                    text = "Ora de " + HomrActivity.orarJSON.getJSONObject("9c").getJSONObject("" + dayValue).optString("" + (hourIndex + 1)) + " incepe peste " + (50 - offset / 60) + " minut si " + (60 - offset % 60) + "secunde.";
+
+                                }
+                            } else {
+                                if (60 - offset % 60 == 1) {
+                                    text = "Ora de " + HomrActivity.orarJSON.getJSONObject("9c").getJSONObject("" + dayValue).optString("" + (hourIndex + 1)) + " incepe peste " + (50 - offset / 60) + " minute si " + (60 - offset % 60) + " secunda.";
+                                } else {
+                                    text = "Ora de " + HomrActivity.orarJSON.getJSONObject("9c").getJSONObject("" + dayValue).optString("" + (hourIndex + 1)) + " incepe peste " + (50 - offset / 60) + " minute si " + (60 - offset % 60) + " secunda.";
+                                }
+                            }
+                        } else {
+                            notify = false;
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         }
         else {
-            text = "Sweet Freedom";
+            notify = false;
         }
 
 
 
+        if(notify) {
+            mBuilder.setOngoing(true);
+            mBuilder.setContentText(text);
+            mBuilder.setContentTitle(title);
+            mBuilder.setSmallIcon(R.drawable.logolzr3);
 
-        mBuilder.setOngoing(true);
-        mBuilder.setContentText(text);
-        mBuilder.setContentTitle(title);
-        mBuilder.setSmallIcon(R.drawable.logolzr3);
+            Notification notification = mBuilder.build();
+            manager.notify(0, notification);
+        }
+    }
 
-        Notification notification = mBuilder.build();
-        manager.notify(0, notification);
-
+    public static int TimeSec(int h, int m, int s){
+        return h*60*60 + m*60 + s;
+    }
+    public static int TimeSec(int h, int m){
+        return h*60*60 + m*60;
     }
 
 }
